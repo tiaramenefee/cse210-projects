a52a9cdbbd89c6d6c4da3c488265576c941188fe
tiara.menefee <men20039@byui.edu>
2025-01-17 21:15:52 -0700

    CPrep 3 changes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+       string adjective = GetAdjective();
+       string noun = GetNoun();
+       
+       int number = Multiply(3, 4);
+       
+        Console.WriteLine($"I looked out the window and saw a {number} {adjective} {noun}s ");
+    }
+
+    static int Multiply (int number1, int nnumber2)
+    {
+        int product = number1 * nnumber2;
+        
+        return product;
+    }
+
+    static string GetAdjective ()
+    {
+        string adjective = "yellow";
+
+        return adjective; 
+    }
+
+    static string GetNoun ()
+    {
+        string word = "bird"; 
+
+        return word;
     }
 }
