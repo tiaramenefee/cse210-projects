9d3a858c1c6415cc3cb0258af16543bfdd559d49
tiara.menefee <men20039@byui.edu>
2025-03-14 21:36:48 -0600

    Foundation1 Part 2

--- final/Foundation1/video.cs
+++ final/Foundation1/video.cs
@@ -5,19 +5,19 @@ public class Video
     public int _length;
     private List<Comment> _comments = new List<Comment>();
 
-    // Method to add a comment to the video
+    
     public void AddComment(Comment comment)
     {
         _comments.Add(comment);
     }
 
-    // Method to display video information along with associated comments
+    
     public void DisplayVideoWithComments()
     {
         Console.WriteLine($"Video: {_title} by {_author}");
         Console.WriteLine("Comments:");
 
-        // Display all comments associated with the video
+        
         foreach (var comment in _comments)
         {
             Console.WriteLine($"- {comment.Name}: {comment.Text}");
